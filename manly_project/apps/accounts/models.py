4dd931b3f73ce52503b5d428bb7f4de70cb4cfcf
Your Name <you@example.com>
2025-12-26 09:48:49 +0530

    super user creation logic and custom manage created

--- manly_project/apps/accounts/models.py
+++ manly_project/apps/accounts/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,PermissionsMixin,BaseUserManager
 
-# Create your models here.
+    )
+
+from django.utils import timezone
+
+
+
+class UserManager(BaseUserManager):
+
+
+    def create_user(self,email,password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email Field must not be empty")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields["is_staff"] = True
+        extra_fields["is_superuser"] = True
+        extra_fields["is_active"] = True
+
+        return self.create_user(email, password, **extra_fields)
+
+
+
+
+class User(AbstractBaseUser ,PermissionsMixin):
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default =False)
+    is_blocked =  models.BooleanField(default=False)
+
+    created_at = models.DateField(default=timezone.now)
+    updated_at = models.DateField(auto_now=True)
+
+    objects = UserManager()
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
+    def __str__ (self):
+        return self.email
+        
+        
--- manly_project/manly_project/settings.py
+++ manly_project/manly_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    
+
     'apps.accounts',
 ]
 
@@ -118,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+AUTH_USER_MODEL = "accounts.User"
