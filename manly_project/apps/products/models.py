b5755a947e6cc2ad6253b99c9dac97ce3c905750
Your Name <you@example.com>
2025-12-27 09:34:53 +0530

    feat :- image crop and zoom added

--- manly_project/apps/products/models.py
+++ manly_project/apps/products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from apps.categories.models import Category
+from PIL import Image
+from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=150)
@@ -23,18 +25,41 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
-class ProductImage(models.Model):
+cclass ProductImage(models.Model):
     product = models.ForeignKey(
         Product,
         on_delete=models.CASCADE,
         related_name="images"
     )
     image = models.ImageField(upload_to="products/")
-    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"Image for {self.product.name}"
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
-    
+        img = Image.open(self.image.path)
 
-# Create your models here.
+        # Convert to RGB (fix PNG / CMYK issues)
+        if img.mode != "RGB":
+            img = img.convert("RGB")
+
+        width, height = img.size
+        min_side = min(width, height)
+
+        # Center crop
+        left = (width - min_side) / 2
+        top = (height - min_side) / 2
+        right = (width + min_side) / 2
+        bottom = (height + min_side) / 2
+
+        img = img.crop((left, top, right, bottom))
+
+        # Resize
+        img = img.resize((800, 800), Image.LANCZOS)
+
+        # Save optimized
+        img.save(
+            self.image.path,
+            quality=85,
+            optimize=True
+        )
+
