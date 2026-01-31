6bf36aa0ebeb09280151ccc9a657ffb5fa4d8fce
Your Name <you@example.com>
2026-01-31 21:32:05 +0530

    fix:-fixed 404.html page for web app

--- manly_project/manly_project/settings.py
+++ manly_project/manly_project/settings.py
@@ -28,9 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
 CSRF_TRUSTED_ORIGINS = [
     'https://*.ngrok.io',
     'https://*.ngrok-free.app',
