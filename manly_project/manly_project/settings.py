dd3471609572cb57e77eb767afd07e81fbf80191
Your Name <you@example.com>
2026-02-02 16:00:36 +0530

    fixed .rnv

--- manly_project/manly_project/settings.py
+++ manly_project/manly_project/settings.py
@@ -142,13 +142,13 @@ WSGI_APPLICATION = 'manly_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-      'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'manly_db',
-        'USER': 'manly_user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
