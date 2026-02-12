1b7e29fcf0e9160267e65e11e4b622d412ec46db
Your Name <you@example.com>
2026-02-12 07:21:42 +0530

    fix:- fixed user login bug

--- manly_project/apps/accounts/views.py
+++ manly_project/apps/accounts/views.py
@@ -284,11 +284,6 @@ def profile_view(request):
 
     return render(request,"account/profile_view.html",{"profile": profile,"breadcrumbs": breadcrumbs,},)
 
-def get_client_ip(request):
-    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
-    if x_forwarded_for:
-        return x_forwarded_for.split(",")[0]
-    return request.META.get("REMOTE_ADDR")
 
 
 
